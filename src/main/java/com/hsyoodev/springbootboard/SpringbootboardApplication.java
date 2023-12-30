22599f243fd7d7d706b7ef72ca869a0abb28f0cf
hsyoodev <hsyoodev@gmail.com>
2023-12-30 16:34:05 +0900

    #9 코드 포멧팅
    줄바꿈을 수정했다.

--- src/main/java/com/hsyoodev/springbootboard/SpringbootboardApplication.java
+++ src/main/java/com/hsyoodev/springbootboard/SpringbootboardApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootboardApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootboardApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootboardApplication.class, args);
+    }
 }
