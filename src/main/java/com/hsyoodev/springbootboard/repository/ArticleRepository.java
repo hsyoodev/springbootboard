ca62513f4a6104f9fd366062808e59be6b8a5b55
hsyoodev <hsyoodev@gmail.com>
2023-12-31 16:04:32 +0900

    #13 Data Rest 설정
    게시글, 댓글의 json api를 자동으로 restful하게 설정

--- src/main/java/com/hsyoodev/springbootboard/repository/ArticleRepository.java
+++ src/main/java/com/hsyoodev/springbootboard/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.hsyoodev.springbootboard.repository;
 
 import com.hsyoodev.springbootboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
