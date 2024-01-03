61af6c19b3d2f4ca5414b432f418862f55dd4c9e
hsyoodev <hsyoodev@gmail.com>
2024-01-03 23:43:52 +0900

    #19 게시글 뷰 기본 골격 작성
    - 컨트롤러, 뷰, 테스트까지 한 번에 적용

--- src/main/java/com/hsyoodev/springbootboard/controller/ArticleController.java
+++ src/main/java/com/hsyoodev/springbootboard/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.hsyoodev.springbootboard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -13,6 +14,16 @@ public class ArticleController {
     @GetMapping
     public String articles(ModelMap modelMap) {
         modelMap.addAttribute("articles", List.of());
+
         return "articles/index";
     }
+
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId, ModelMap modelMap) {
+        modelMap.addAttribute("article", "article"); // TODO: 구현할 때 실제 데이터를 넣어줘야 한다.
+        modelMap.addAttribute("articleComments", List.of());
+
+        return "articles/detail";
+    }
+
 }
